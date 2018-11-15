162afba3a2c11dfd91ff13702f1b9ecaf06bc811
Raool <838326280@qq.com>
2018-11-15 22:19:52 +0800

    完成选择答案功能

--- src/domain.js
+++ src/domain.js
@@ -1,4 +1,4 @@
-const domain = 'http://139.219.13.39:8086';
-// const domain = 'http://localhost:8086';
+// const domain = 'http://139.219.13.39:8086';
+const domain = 'http://localhost:8086';
 
 export default domain;
